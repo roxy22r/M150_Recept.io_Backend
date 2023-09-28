ed2019328caed7f28c0fc6a842c3d1db364d17a6
roxy22r <raksana280@gmail.com>
2023-09-28 16:47:40 +0200

    Added Extentions

---
+++ RezeptIO/RecipeRepository/IngredientsExtention.cs
@@ -0,0 +1,21 @@
+﻿using Rep = RecipeRepositories.Models;
+using Svc = RecipeService.Models;
+namespace RezeptIO.API
+{
+    public static class IngredientsExtention
+    {
+        public static List<Rep.Ingredient> ToIngredient(List<Svc.Ingredient> ingredient)
+        {
+            return ingredient.Select(ingredientItem => { return ToIngredient(ingredientItem); }).ToList();
+
+        }
+        public static Rep.Ingredient ToIngredient(this Svc.Ingredient ingredient)
+        {
+            return new Rep.Ingredient
+            {
+                Name = ingredient.Name,
+                Quantity = ingredient.Quantity,
+            };
+        }
+    }
+}
---
+++ RezeptIO/RecipeRepository/RecipeExtention.cs
@@ -0,0 +1,21 @@
+﻿using Rep=  RecipeRepositories.Models;
+using Svc = RecipeService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeRepositories
+{
+    public static class RecipeExtention
+    {
+        public static Rep.Recipe toRecipe (Svc.Recipe recipe) {
+            return new Rep.Recipe() { 
+                Id = recipe.Id,
+                Title = recipe.Title,
+                Ingredients = recipe.Inde,
+            };
+        }
+    }
+}
