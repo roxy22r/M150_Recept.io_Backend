41135889ee6ae645c6aef30f4f1c8e6cd6815e23
roxy22r <raksana280@gmail.com>
2023-09-28 16:47:40 +0200

    Added Extentions

--- RezeptIO/RecipeTest/RecipeServiceTest.cs
+++ RezeptIO/RecipeTest/RecipeServiceTest.cs
@@ -10,31 +10,7 @@ namespace RecipeTest
     {
         private  readonly IRecipeService testee;
 
-        public RecipeServiceTest(IRecipeService recipeS) {
-        testee = recipeS;
-        }
-
-        [Fact]
-        public void updateRecipe_correctFormmatedRecipe_200RepsoneCode()
-        {
-            Recipe recipe = new Recipe
-            {
-                Title = "Apple Pie",
-                Description = "A Pie Made with Apples"
-                
-            };
-          //  recipesCollection.InsertOne(recipe);
-            Recipe updatedRecipe = new Recipe
-            {
-                Id = recipe.Id,
-                Title = "Apple Pie 5",
-                Description = "New Pie Descritpion"
-            };
-         testee.UpdateRecipe(updatedRecipe);
-
-        //    Recipe result = recipesCollection.Find(r => r.Id.Equals(recipe.Id)).First();
-        //    Assert.Equal(result.Description,recipe.Description ) ;
-        }
+ 
 
 
     }
---
+++ RezeptIO/RezeptIO/IngredientsExtention.cs
@@ -0,0 +1,19 @@
+﻿using Api = RezeptIO.API.Controllers.Models;
+using Svc = RecipeService.Models;
+namespace RezeptIO.API
+{
+    public static class IngredientsExtention
+    {
+        public static List<Api.Ingredient> ToIngredient(List<Svc.Ingredient> ingredient) {
+          return ingredient.Select(ingredientItem => { return ToIngredient(ingredientItem); }).ToList();
+           
+        }
+        public static Api.Ingredient ToIngredient(this Svc.Ingredient ingredient){
+            return new Api.Ingredient
+            {
+                Name = ingredient.Name,
+                Quantity = ingredient.Quantity,
+            };
+        }
+    }
+}
