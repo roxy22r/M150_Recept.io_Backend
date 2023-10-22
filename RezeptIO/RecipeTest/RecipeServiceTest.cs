4c6b8663ea97ddb728965d237d31f1663a642c85
roxy22r <raksana280@gmail.com>
2023-10-22 16:30:27 +0200

    Added Testcases for get,getAll and added 3a=>arrange,act,assert

--- RezeptIO/RecipeTest/RecipeServiceTest.cs
+++ RezeptIO/RecipeTest/RecipeServiceTest.cs
@@ -2,6 +2,7 @@ using MongoDB.Driver;
 using RecipeService;
 using RecipeService.Models;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 
@@ -19,18 +20,72 @@ namespace RecipeTest
         [Fact]
         public void createRecipe_titleAndIngredensAreSet_idIsNotEmptyOrNull()
         {
+            //arrange
             var ingredients=CreateTestIngredientData();
             Recipe recipe=  new Recipe()
             {
                 Title = "Pizza",
                 Ingredients = ingredients,
             };
+            //act
             var result = Testee.CreateRecipe(recipe);
-           Assert.True(string.IsNullOrEmpty(result.Item2.Id));
+            //assert
+            Assert.True(string.IsNullOrEmpty(result.Item2.Id));
+        }
+
+        [Fact]
+        public void getRecipe_recipeExists_getRecipe()
+        {
+            //arrange
+            var ingredients = CreateTestIngredientData();
+            Recipe recipe = new Recipe()
+            {
+                Title = "Pizza",
+                Ingredients = ingredients,
+            };
+            var createdRecipe = Testee.CreateRecipe(recipe).Item2;
+            //act
+            var item=Testee.GetRecipe(createdRecipe.Id).Item2;
+            //assert
+            Assert.Equal(item, createdRecipe);
+        }
+        [Fact]
+        public void getRecipe_recipeDoesNotExists_getRecipe()
+        {
+            //arrange
+            var ingredients = CreateTestIngredientData();
+            Recipe recipe = new Recipe()
+            {
+                Title = "Pizza",
+                Ingredients = ingredients,
+            };
+            var createdRecipe = Testee.CreateRecipe(recipe);
+            //act
+            var result = Testee.GetAllRecipes().Item2.First();
+            //assert
+            Assert.Equal(result, createdRecipe.Item2);
+        }
+
+        [Fact]
+        public void getRecipeAll_recipeExists_getAllRecipe()
+        {
+            //arrange
+            var ingredients = CreateTestIngredientData();
+            Recipe recipe = new Recipe()
+            {
+                Title = "Pizza",
+                Ingredients = ingredients,
+            };
+            var createdRecipe = Testee.CreateRecipe(recipe);
+            //act
+            var item = Testee.GetRecipe(createdRecipe.Item2.Id);
+            //assert
+            Assert.Equal(item, createdRecipe);
         }
         [Fact]
         public void updateRecipe_recipeExistsAndTitleIsChanged_titleIsUpdate()
         {
+            //arrange
             const string updatedTitle = "Updated";
             var ingredients = CreateTestIngredientData();
             Recipe recipe = new Recipe()
@@ -38,8 +93,10 @@ namespace RecipeTest
                 Title = "Pizza",
                 Ingredients = ingredients,
             };
+            //act
             var newRecipe = Testee.CreateRecipe(recipe).Item2;
             newRecipe.Title = updatedTitle;
+            //assert
             Recipe result = Testee.UpdateRecipe(newRecipe).Item2;
             Assert.Equal(result.Title, updatedTitle);
 
@@ -48,13 +105,14 @@ namespace RecipeTest
         [Fact]
         public void deleteRecipe_recipeExistsDeleteExistingRecipe_isDeletetedSuccess()
         {
-            const string updatedTitle = "Updated";
+            //arrange
             var ingredients = CreateTestIngredientData();
             Recipe recipe = new Recipe()
             {
                 Title = "Pizza",
                 Ingredients = ingredients,
             };
+            //act
             var newRecipe = Testee.CreateRecipe(recipe).Item2;
             RecipeServiceResponse result = Testee.DeleteRecipe(newRecipe.Id).Result;
             Assert.Equal(result,RecipeServiceResponse.Success);
@@ -62,8 +120,9 @@ namespace RecipeTest
         [Fact]
         public void deleteRecipe_recipeDoesNotExistsDeleteExistingRecipe_isDeletetedFailure()
         {
-        
+            //act
             RecipeServiceResponse result = Testee.DeleteRecipe("1").Result;
+            //assert
             Assert.Equal(result, RecipeServiceResponse.Failure);
         }
 
